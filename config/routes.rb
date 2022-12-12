908e06f5ccd1ab24ca206f8f1724933a522c3652
EC2 Default User <ec2-user@ip-172-31-28-191.ec2.internal>
2022-12-12 20:02:50 +0000

    [Update] 4-12 {Necessary books controllers provided.}

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/' => 'homes#top'
+  # のちにget '/' => 'homes#top'以外をresources :booksに変更
+  get 'books' => 'books#index'
+  get 'books/id' => 'books#show'
+  get 'books/id/edit' => 'books#edit'
 end
